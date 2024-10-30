593a8e351383064671965d5bdcd76e29caaf4852
MayLajnef <125302971+MayLajnef@users.noreply.github.com>
2024-10-30 13:11:09 +0100

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -11,6 +11,34 @@ void lire_notes(int N, int POINTS[]) {
     }
 }
 
+int note_max(int N, int POINTS[]) {
+    int max = POINTS[0];
+    for (int i = 1; i < N; i++) {
+        if (POINTS[i] > max)
+            max = POINTS[i];
+    }
+    return max;
+}
+
+int note_min(int N, int POINTS[]) {
+    int min = POINTS[0];
+    for (int i = 1; i < N; i++) {
+        if (POINTS[i] < min)
+            min = POINTS[i];
+    }
+    return min;
+}
+
+float moyenne_notes(int N, int POINTS[]) {
+    int somme = 0;
+    float moyenne;
+    for (int i = 0; i < N; i++) {
+        somme += POINTS[i];
+    }
+    moyenne = somme / (float) N;
+    return moyenne;
+}
+
 
 
 int main(int argc, char* argv[]) {
@@ -23,5 +51,10 @@ int main(int argc, char* argv[]) {
     for (int i=0; i < N; i++) {
         printf("POINTS[%d] = %d\n", i, POINTS[i]);
     }
+
+    printf("La note maximale du devoir SR01 est de %d.\n", note_max(N, POINTS));
+    printf("La note minimale du devoir SR01 est de %d.\n", note_min(N, POINTS));
+    printf("La moyenne des notes du devoir SR01 est de %.2f.\n", moyenne_notes(N, POINTS));
+
     return 0;
 }
