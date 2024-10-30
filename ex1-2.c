78d05309473559b5f45e5a6af42c478875e95a10
MayLajnef <125302971+MayLajnef@users.noreply.github.com>
2024-10-30 13:44:51 +0100

    Update ex1-2.c

--- ex1-2.c
+++ ex1-2.c
@@ -14,8 +14,7 @@ Pour calculer A&&B ||!0&&C++&&!D++, on réalise chaque opération en respectant 
 3) ET logique : A&&B = 20&&5 = 1 et !0&&C++&&!D++ = 1&&(-9)&&1 = 1
 4) OU logique : A&&B ||!0&&C++&&!D++ = 1 || 1 = 1
 Donc, A&&B ||!0&&C++&&!D++ = 1
-Ensuite, comme on applique à C et à D des incrémentations postfixées,
-leurs valeurs restent inchangées.
+Ensuite, comme on applique à C et à D des post-incrémentations, leurs valeurs restent inchangées.
 Donc, C = -10 et D = 2.
 C'est ce qui explique le résultat obtenu à l'exécution :
 
